1a7a02a9fb083a7f6c3de5b718e441655a43d092
SpaceWD <121475790+motherfortress2@users.noreply.github.com>
2024-11-02 10:59:15 -0400

    Update crosswalk.js

--- crosswalk.js
+++ crosswalk.js
@@ -1,8 +1,8 @@
-// these are the variables required for the game to launch. Do not use the example account provided, make your own username.
+// these are the variables required for the game to launch. Do not use the example account provided, make your own username. Keep test as the password.
 let emulateServer = Blizzard;
 let blizzardServer = emulateServer + port;
 const username = "SpaceWD";
-var password = "";
+var password = "test";
 const penguinColor = "2";
 const port = 6114
 const era = "AS1"
